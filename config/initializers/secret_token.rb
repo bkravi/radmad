b39f8bf7b8930ae3ac8cc3bc788566c6862eeddd
Ravi OS <bkravi.os@gmail.com>
2011-12-26 22:35:56 +0530

    Initial Commit: Created new application "radmad"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Radmad::Application.config.secret_token = 'cbe490e9fc3dff5a4abbd901f820b38aba31624ab1511890e8f3857d9f1c886c42c0b9ba992ec433947471b84e2c50d597939f347caa5a41a08082e3d3a6996a'
